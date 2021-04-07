94a923d87466b225354dc25e05db94531d855c7d
bhuvaneswaran1920 <babubhuvaneswaran@gmail.com>
2021-04-07 17:17:29 +0530

    43CCDA2

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html");
 })
 
-app.listen(process.env.PORT, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("server started");
 });
